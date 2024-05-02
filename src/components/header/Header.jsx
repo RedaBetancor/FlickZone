0ae579dc6e87337df3f6718a21f599519e7e3a51
RedaBetancor <mohamedredazinatharrounbetancor@alumno.ieselrincon.es>
2024-05-02 09:39:31 +0100

    First Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+
+import "./Header.css";
+
+function Header() {
+	return (
+		<div>
+			<header className="header">
+				<img src="../../public/fotos/FlickZone.png" alt="Logo" id="header-logo"/>
+				<button className="signInButton">Iniciar sesión</button>
+			</header>
+		</div>
+	)
+}
+
+export default Header;
